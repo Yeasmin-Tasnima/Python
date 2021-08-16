515255fd7fee20fdba522719ec1cb7703edacd76
Tasnima Yeasmin <tasnima.yeasmin@bjitgroup.com>
2021-08-16 11:23:30 +0600

    Set, General format, function call, Frozen set, equations

--- learn_python/set.py
+++ learn_python/set.py
@@ -1,2 +1,21 @@
+# function call with set literal
 ids = set([1, 2, 1, 3, 2, 4, 5])
 print(ids)
+
+# general format
+ids = {1, 2, 1, 3, 2, 4, 5}
+print(ids)
+
+# frozen set
+lst = [1, 2]
+fs = frozenset(lst)
+# couldn't add elements to a frozenset
+print(fs)
+
+# mathematical equations
+x = {1, 2, 3}
+y = {3, 4}
+
+print('Union, x | y: {}'.format(x | y))
+print('Intersection, x & y: {}'.format(x & y))
+print('Subtraction, x - y: {}'.format(x - y))
